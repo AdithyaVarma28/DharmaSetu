9e451f264ca9a621f0c2a07434de6283b9b98677
Abhinavpapini <papiniabhinav@gmail.com>
2025-04-09 20:44:46 +0530

    backend finalized

--- backend/app.py
+++ backend/app.py
@@ -2,22 +2,23 @@ from modules.query import correct_legal_text
 from modules.generate import get_most_relevant_doc_id, get_cleaned_document_by_id
 from modules.summarize import summarize_legal_text
 
-def run_legal_chatbot():
-    while True:
-        query=input("Enter your legal query (or 'exit' to quit): ")
-        clean_query=correct_legal_text(query)
-        if query.lower()=='exit':
-            break
+def run_legal_chatbot(query):
+    clean_query = correct_legal_text(query)
 
-        doc_id=get_most_relevant_doc_id(clean_query)
-        if doc_id:
-            content=get_cleaned_document_by_id(doc_id)
-            if not content:
-                print("Could not extract document content.")
-        else:
-            print("No relevant document found.")
+    doc_id = get_most_relevant_doc_id(clean_query)
+    if doc_id:
+        content = get_cleaned_document_by_id(doc_id)
+        if not content:
+            # if content is empty
+            return "No relevant content found in the document."
+    else:
+        # if you don't get doc_id
+        return "No relevant document found for the query."
 
-        print("Answer: ",summarize_legal_text(content))
+    answer = summarize_legal_text(content)
 
-if __name__=="__main__":
-    run_legal_chatbot()
+    if answer:
+        return answer
+    else:
+        # if you don't get answer
+        return "Unable to generate a summary for the document."
