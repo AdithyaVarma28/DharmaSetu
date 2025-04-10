b12d5ff2297a3f47bf24dce753c973f0fd9635a9
Adithya Varma Kusampudi <161280534+AdithyaVarma28@users.noreply.github.com>
2025-04-10 10:43:12 +0530

    updated config.py

--- backend/modules/config.py
+++ backend/modules/config.py
@@ -1,10 +1,8 @@
-import os
+API_KEY="hf_aDBBRSIpuDsDymoPymIomnnHSFtxZFoQUkui"
+API_URL="https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3-8B-Instruct"
 
-API_KEY = os.getenv("HUGGINGFACE_API_KEY", "default_placeholder")
-API_URL = os.getenv("HUGGINGFACE_API_URL", "https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3-8B-Instruct")
+KANOON_AUTH_TOKEN="8ed2cc784d1466c8481f695a93106607163253a5"
+KANOON_BASE_URL="https://api.indiankanoon.org/"
 
-KANOON_AUTH_TOKEN = os.getenv("KANOON_AUTH_TOKEN", "default_placeholder")
-KANOON_BASE_URL = os.getenv("KANOON_BASE_URL", "https://api.indiankanoon.org/")
-
-GROQ_KEY = os.getenv("GROQ_API_KEY", "default_placeholder")
-GROQ_API_URL = os.getenv("GROQ_API_URL", "https://api.groq.com/openai/v1/chat/completions")
+GROQ_KEY="gsk_PbIAM2NEi7YGDwxcX7soWGbdy3PYifJkG1pMSXSRcAmIezfZ0O"
+GROQ_API_URL="https://api.groq.com/openai/v1/chat/completions"
