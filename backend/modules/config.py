db2adfcdeb565f9ffa16ac43a64ec255010c96ef
Adithya Varma Kusampudi <161280534+AdithyaVarma28@users.noreply.github.com>
2025-05-17 20:09:38 +0530

    updated config.py

--- backend/modules/config.py
+++ backend/modules/config.py
@@ -1,8 +1,8 @@
-API_KEY="hf_aDBBRSIpuDsDymoPymIomnnHSFtxZFoQUkui"
+API_KEY="YOUR_API_KEY"
 API_URL="https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3-8B-Instruct"
 
-KANOON_AUTH_TOKEN="8ed2cc784d1466c8481f695a93106607163253a5"
+KANOON_AUTH_TOKEN="YOUR_API_KEY"
 KANOON_BASE_URL="https://api.indiankanoon.org/"
 
-GROQ_KEY="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"
+GROQ_KEY="YOUR_API_KEY"
 GROQ_API_URL="https://api.groq.com/openai/v1/chat/completions"
